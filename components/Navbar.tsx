cf022652be894e925ad568c6a7865cc267fab061
utkarsh seth <utkarshseth2019@gmail.com>
2022-01-24 20:54:21 +0530

    fixing navbar links hover color and remove font semibold

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,8 @@ function Navbar() {
     console.log(showMe)
     // repeat styling  
     const gap = 'gap-10'
-    const linkHover = 'hover:opacity-50  md:hover:text-Violet'
+    const linkHover = '  hover:text-Violet  '
+
     return (
         <>
             <nav className={'  py-10 md:py-0 px-8 md:px-20 flex items-center  justify-between md:justify-start max-w-7xl mx-auto text-lg  '}>
@@ -23,7 +24,7 @@ function Navbar() {
                     <img src="/images/logo.svg" alt="" />
                 </div>
                 {/* 2 */}
-                <div className={`${showMe ? '' : 'hidden'}    md:flex absolute md:static     md: items-center  rounded-xl    top-24  bg-Violet  md:bg-transparent  font-semibold  md:md:text-gray-400 text-white md:justify-between md:w-full flex-col md:flex-row  inset-x-0 mx-8 z-50`}>
+                <div className={`${showMe ? '' : 'hidden'}    md:flex absolute md:static     md: items-center  rounded-xl    top-24  bg-Violet  md:bg-transparent   md:md:text-gray-400 text-white md:justify-between md:w-full flex-col md:flex-row  inset-x-0 mx-8 z-50 `}>
                     {/* 1 */}
                     {/* <div className='bg-yellow-400'> */}
                     <div className={`links   flex flex-col   items-center gap-6 my-8  md:flex-row  md:${gap}  `}>
